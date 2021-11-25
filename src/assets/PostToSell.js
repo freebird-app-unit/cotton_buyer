7bb6f59a4274774e14834f1f43591c5913ec92ee
Freebird <81305380+freebird-app-unit@users.noreply.github.com>
2021-11-25 13:36:39 +0530

    Update post to buy icon - Bhavin

--- src/assets/PostToSell.js
+++ src/assets/PostToSell.js
@@ -1,55 +1,58 @@
 import * as React from "react"
-import Svg, { Path } from "react-native-svg"
+import Svg, { Path, Circle } from "react-native-svg"
 
 function PostToSell_Icon(props) {
   return (
     <Svg
-      data-name="Group 63"
+      data-name="Group 1109"
       xmlns="http://www.w3.org/2000/svg"
-      width={28}
-      height={28}
-      viewBox="0 0 28 28"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
       {...props}
     >
-      <Path data-name="Path 109" d="M0 0h28v28H0z" fill="none" />
       <Path
-        data-name="Path 110"
-        d="M19 6.5h3.75A1.25 1.25 0 0124 7.75V21.5a2.5 2.5 0 01-5 0V5.25A1.25 1.25 0 0017.75 4H5.25A1.25 1.25 0 004 5.25v15A3.75 3.75 0 007.75 24H21.5"
+        data-name="Path 307"
+        d="M17 17H6V3H4"
+        fill="none"
+        stroke="#69ba53"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
+      />
+      <Path data-name="Path 306" d="M0 0h24v24H0z" fill="none" />
+      <Circle
+        data-name="Ellipse 350"
+        cx={2}
+        cy={2}
+        r={2}
+        transform="translate(4 17)"
         fill="none"
         stroke="#2c3e50"
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth={2}
+        strokeWidth={1.5}
+      />
+      <Circle
+        data-name="Ellipse 351"
+        cx={2}
+        cy={2}
+        r={2}
+        transform="translate(15 17)"
+        fill="none"
+        stroke="#2c3e50"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
       />
       <Path
-        data-name="Line 20"
-        transform="translate(9.6 9.333)"
+        data-name="Path 308"
+        d="M6 5l14 1-1 7H6"
         fill="none"
         stroke="#69ba53"
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth={2}
-        d="M0 0L4 0"
-      />
-      <Path
-        data-name="Line 21"
-        transform="translate(9.6 14)"
-        fill="none"
-        stroke="#69ba53"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M0 0L4 0"
-      />
-      <Path
-        data-name="Line 22"
-        transform="translate(9.6 18.667)"
-        fill="none"
-        stroke="#69ba53"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M0 0L4 0"
+        strokeWidth={1.5}
       />
     </Svg>
   )
